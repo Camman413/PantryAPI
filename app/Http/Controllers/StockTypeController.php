be667a376833916f7413748d1b264a34440b94bd
Cameron Hale <cameron@haletek.com>
2022-03-27 16:13:58 -0600

    Fixed case on StockType

--- app/Http/Controllers/StockTypeController.php
+++ app/Http/Controllers/StockTypeController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\StockTypeResource;
-use App\Models\stockType;
-use App\Repositories\stockTypeRepository;
+use App\Models\StockType;
+use App\Repositories\StockTypeRepository;
 use Illuminate\Http\Request;
 
 class StockTypeController extends Controller
@@ -15,9 +15,9 @@ class StockTypeController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    private stockTypeRepository $stockTypeRepository;
+    private StockTypeRepository $stockTypeRepository;
 
-    public function __construct(stockTypeRepository $stockTypeRepository)
+    public function __construct(StockTypeRepository $stockTypeRepository)
     {
         $this->stockTypeRepository = $stockTypeRepository;
     }
@@ -45,10 +45,10 @@ class StockTypeController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\stockType  $stockType
+     * @param  \App\Models\StockType  $StockType
      * @return \Illuminate\Http\Response
      */
-    public function show(stockType $stockType)
+    public function show(StockType $stockType)
     {
         return new StockTypeResource($this->stockTypeRepository->getItemById($stockType->id));
     }
@@ -57,7 +57,7 @@ class StockTypeController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\stockType  $stockType
+     * @param  \App\Models\StockType  $StockType
      * @return \Illuminate\Http\Response
      */
     public function update(stocktype $stockType, Request $request)
@@ -73,10 +73,10 @@ class StockTypeController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\stockType  $stockType
+     * @param  \App\Models\StockType  $StockType
      * @return \Illuminate\Http\Response
      */
-    public function destroy(stockType $stockType)
+    public function destroy(StockType $stockType)
     {
         $this->stockTypeRepository->deleteItem($stockType->id);
     }
--- app/Repositories/stockTypeRepository.php
+++ app/Repositories/stockTypeRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repositories;
 
-use App\Models\stockType;
+use App\Models\StockType;
 
 class stockTypeRepository extends Repository
 {
     public function getAllItems(){
-        return stockType::all();
+        return StockType::all();
     }
     public function getItemById(int $id){
-        return stockType::findOrFail($id);
+        return StockType::findOrFail($id);
     }
     public function createItem(array $details)
     {
-        return stockType::create($details);
+        return StockType::create($details);
     }
     public function updateItem(int $id, array $details)
     {
-        return stockType::whereId($id)->update($details);
+        return StockType::whereId($id)->update($details);
     }
     public function deleteItem(int $id)
     {
-        stockType::destroy($id);
+        StockType::destroy($id);
     }
 }
